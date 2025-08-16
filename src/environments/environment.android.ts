c5643fbd87a234af802b33bf41e293ba5becbc68
Andy Ghillebaert <ghillebaert.andy@gmail.com>
2025-08-16 16:18:40 +0200

    Divers corrections

--- src/environments/environment.android.ts
+++ src/environments/environment.android.ts
@@ -4,7 +4,7 @@ import { IEnvironment } from 'src/environments/i-environment';
 export const environment: IEnvironment = {
   ...environmentDefault,
   production: true,
-  domaine: 'meteo.ics.corp',
+  domaine: 'meteo.cellarmoon.fr',
   get apiUrl(): string {
     return `https://${this.domaine}${this.apiPath}`;
   },
